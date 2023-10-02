9cb7b4df2a208b11967548b85fb6de40563a6e61
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2023-10-02 15:15:25 -0400

    dfgagd

--- Demo 5A/1.2.3.cpp
+++ Demo 5A/1.2.3.cpp
@@ -6,6 +6,27 @@ using namespace std;
 
 void CopierFichier(string Chemin1, string Chemin2)
 {
+	fstream Fichier1;
+	fstream Fichier2;
+	Fichier1.open(Chemin1, ios::in);
+	Fichier2.open(Chemin2, ios::out); // créer et ecraser
 
+	if (Fichier1.fail())
+	{
+		cout << "Erreur ouverture";
+		exit(EXIT_FAILURE);
+	}
+
+	char Caract = Fichier1.get();
+	while (!Fichier1.eof())
+	{
+
+
+
+
+	}
+
+	Fichier1.close();
+	Fichier2.close();
 
 }
